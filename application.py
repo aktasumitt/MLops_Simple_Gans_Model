b34e05009c9b8369c74e34fd69846e23fb1380af
Ümit Aktaş <152315417+aktasumitt@users.noreply.github.com>
2025-02-27 22:09:24 +0300

    Update application.py

--- application.py
+++ application.py
@@ -25,5 +25,5 @@ def index():
     return render_template('index.html', image_paths=image_paths)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(host='0.0.0.0', port=8080) 
 
